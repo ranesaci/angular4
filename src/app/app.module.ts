8656f5c18bbbaf1174b56d9301e8db7cdb2bc08c
sachinrane <sachin.rane@vuclip.com>
2017-07-23 20:53:29 +0530

    added component routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path: 'path4Comp1',
+        component: Comp1Component
+      },
+      {
+        path: 'path4Comp2',
+        component: Comp2Component
+      }
+    ])
+
   ],
   providers: [],
   bootstrap: [AppComponent]
