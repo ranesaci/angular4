dba45171141793299dcc6de70ce5e9a755702152
sachinrane <sachin.rane@vuclip.com>
2017-07-24 11:59:06 +0530

    added data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app start!!';
+  title = 'AppComponent title value**!!';
+  employee = {
+    id: '101',
+    name: 'Sachin'
+  };
+
+  trueFalseVar= false;
+  name= 'sachin'  ;
+  list = ['Sachin', 'Rane', 'xyz','1111','2222222222'];
+  addItem = '';
+  pushItemInList = function(){
+       if (this.addItem != null) {
+          this.list.push(this.addItem);
+         this.addItem = '';
+
+       }
+  };
+  removeItemInList = function(index){
+    this.list.splice(index,1);
+  }
+
 }
